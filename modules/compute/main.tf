71b417e0ea922d263793ee15249dc21acbc48264
rlfrjs3 <rlfrjs3@naver.com>
2026-07-16 10:34:49 +0900

    20260716

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -81,7 +81,7 @@ resource "aws_autoscaling_group" "web_asg" {
   vpc_zone_identifier       = var.private_subnet_ids           #프라이빗 서브넷에만 생성
   health_check_type         = "ELB"                            #웹서버 80포트 응답 없으면 삭제하고 재생성
   health_check_grace_period = 300
-#  instance_refresh { strategy = "Rolling" }              #시작템플릿 설정이 바뀐다면 기존 인스턴스 종료하고 다시 생성
+  #  instance_refresh { strategy = "Rolling" }              #시작템플릿 설정이 바뀐다면 기존 인스턴스 종료하고 다시 생성
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
@@ -135,7 +135,7 @@ resource "aws_lb_target_group" "web_tg" {
   health_check {
     path                = "/" #루트 경로로 HTTP 체크
     interval            = 60  #60초마다 체크
-    timeout             = 10   #응답대기 5초
+    timeout             = 10  #응답대기 5초
     healthy_threshold   = 3   #2번 연속 성공해야 정상으로 판단
     unhealthy_threshold = 3   #2번 연속 실패하면 비정상으로 판단
   }
